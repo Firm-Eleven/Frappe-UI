2d4c31aca56a37cefd80bbb49397fee89c04feb7
hanan-me <142660158+hanan-me@users.noreply.github.com>
2026-04-01 17:10:44 +0500

    Update api.py

--- crm/fcrm/doctype/crm_doc/api.py
+++ crm/fcrm/doctype/crm_doc/api.py
@@ -6,11 +6,11 @@ from crm.fcrm.doctype.crm_form_script.crm_form_script import get_form_script
 
 @frappe.whitelist()
 def get_doc(name):
-	doc = frappe.get_doc("CRM Doc", name)
+	doc = frappe.get_doc("Crm Doc", name)
 	doc.check_permission("read")
 
 	doc = doc.as_dict()
 
-	doc["fields_meta"] = get_fields_meta("CRM Doc")
-	doc["_form_script"] = get_form_script("CRM Doc")
+	doc["fields_meta"] = get_fields_meta("Crm Doc")
+	doc["_form_script"] = get_form_script("Crm Doc")
 	return doc
