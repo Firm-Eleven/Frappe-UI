9a81b95e6b33cd1f579e46d6431f1afb385f5c8c
hanan-me <142660158+hanan-me@users.noreply.github.com>
2026-04-02 11:28:13 +0500

    Update RenderForm.vue

--- frontend/src/pages/RenderForm.vue
+++ frontend/src/pages/RenderForm.vue
@@ -26,6 +26,7 @@
 import { useRoute } from 'vue-router'
 import { computed } from 'vue'
 import FormModel from '@/components/Modals/FormModal.vue'
+import Breadcrumbs from '@/components/Breadcrumbs.vue'
 
 const route = useRoute()
 
