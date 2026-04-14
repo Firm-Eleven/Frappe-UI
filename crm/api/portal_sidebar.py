7fdbdcb91993f4bd3f9b4ca61a353175e2caf8ee
hanan-me <142660158+hanan-me@users.noreply.github.com>
2026-04-14 15:46:49 +0500

    Update portal_sidebar.py

--- crm/api/portal_sidebar.py
+++ crm/api/portal_sidebar.py
@@ -3,5 +3,5 @@ import frappe
 
 @frappe.whitelist()
 def get_sidebar_items():
-  sidebar_items = frappe.get_all("Sidebar Item", filters={"disable":0}, fields=['document'])
+  sidebar_items = frappe.get_all("SideBar Item", filters={"disable":0}, fields=['document'])
   return sidebar_items
